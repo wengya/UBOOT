07fdc8fa9ac9eb3729431a88126fabd2444576fe
wy <102970833@qq.com>
2021-04-10 13:13:34 -0400

    upload files

---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,4 @@
+#define U_BOOT_DATE "Jan 31 2021"
+#define U_BOOT_TIME "10:56:45"
+#define U_BOOT_TZ "-0500"
+#define U_BOOT_DMI_DATE "01/31/2021"
---
+++ include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2017.03"
+#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
+#define CC_VERSION_STRING "arm-linux-gnueabihf-gcc (Linaro GCC 6.2-2016.11) 6.2.1 20161016"
+#define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2016.11) 2.27.0.20161019"
